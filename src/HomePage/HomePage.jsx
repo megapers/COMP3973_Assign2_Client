8dcf24c6af9b697261e152c9d07f681fe1f835e8
TMakimov <TMakimov@DTMAKIMOVW10>
2019-11-22 16:39:19 -0800

    Initial

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { userService, authenticationService } from '@/_services';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: authenticationService.currentUserValue,
+            users: null
+        };
+    }
+
+    componentDidMount() {
+        //userService.getAll().then(users => this.setState({ users }));
+    }
+
+    render() {
+        const { currentUser, users } = this.state;
+        return (
+            <div>
+                <h1>COMP3973 Assignment2</h1>
+                <h2>Implemented by: Timur Makimov, A00903109</h2>
+                <h3>Hi {currentUser.firstName}!</h3>
+                <p>You're logged in with React & JWT!!</p>
+                {/* <h3>Users from secure api end point:</h3>
+                {users &&
+                    <ul>
+                        {users.map(user =>
+                            <li key={user.id}>{user.firstName} {user.lastName}</li>
+                        )}
+                    </ul>
+                } */}
+            </div>
+        );
+    }
+}
+
+export { HomePage };
